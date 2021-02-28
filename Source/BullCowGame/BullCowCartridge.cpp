289370bce3e7a2d5199471f8396615085b71dfa3
Cancain <eriksson.tomas@gmail.com>
2021-02-28 18:06:10 +0100

    Lecture 61

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -2,53 +2,72 @@
 #include "BullCowCartridge.h"
 #include <String>
 
-void UBullCowCartridge::BeginPlay(){ // When the game starts
+void UBullCowCartridge::BeginPlay()
+{ // When the game starts
     Super::BeginPlay();
     SetupGame();
 
-    // PrintLine(FString::Printf(TEXT("The hidden word is %s"), *HiddenWord)); // Overridden in Cartridge.h 
+    // PrintLine(FString::Printf(TEXT("The hidden word is %s"), *HiddenWord)); // Overridden in Cartridge.h
     PrintLine(TEXT("The hidden word is %s."), *HiddenWord); // Debug line
 }
 
-void UBullCowCartridge::OnInput(const FString& input){ // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input)
+{ // When the player hits enter
     // If the game is over ClearScreen() and SetupGame() the game
     // Else check player guess
 
-    if(bGameOver){
+    if (bGameOver)
+    {
         ClearScreen();
         SetupGame();
-    } else {
-           if(input == HiddenWord){
-            PrintLine(TEXT("You have won!"));
-            EndGame();
-        } else {
-            PrintLine(TEXT("Lost a life!"));
-            PrintLine(TEXT("%i"), --Lives);
-            if(Lives > 0){
-                if(input.Len() != HiddenWord.Len()){
-                    PrintLine(TEXT("Sorry, try guessing again!.\nYou have %i lives"), Lives);
-                }
-            } else {
-                PrintLine(TEXT("You have no lives left"));
-                EndGame();
-            }
-        }
+    }
+    else
+    {
+        ProcessGuess(Input);
     }
 }
 
-void UBullCowCartridge::SetupGame(){
-    PrintLine(TEXT("Welcome to BullCow!"));  
+void UBullCowCartridge::SetupGame()
+{
+    PrintLine(TEXT("Welcome to BullCow!"));
 
     HiddenWord = TEXT("mouse");
     Lives = HiddenWord.Len();
     bGameOver = false;
 
-    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len()); 
-    PrintLine(TEXT("You have %i lives."), Lives); 
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives."), Lives);
     PrintLine(TEXT("Type in your guess and \nPress enter to continue..."));
 }
 
-void UBullCowCartridge::EndGame(){
+void UBullCowCartridge::EndGame()
+{
     bGameOver = true;
     PrintLine(TEXT("Press enter to play again"));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+        EndGame();
+    }
+    else
+    {
+        PrintLine(TEXT("Lost a life!"));
+        PrintLine(TEXT("%i"), --Lives);
+        if (Lives > 0)
+        {
+            if (Guess.Len() != HiddenWord.Len())
+            {
+                PrintLine(TEXT("Sorry, try guessing again!.\nYou have %i lives"), Lives);
+            }
+        }
+        else
+        {
+            PrintLine(TEXT("You have no lives left"));
+            EndGame();
+        }
+    }
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,18 +6,19 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+	virtual void OnInput(const FString &Input) override;
 	void SetupGame();
 	void EndGame();
+	void ProcessGuess(FString Guess);
 
-	private:
+private:
 	int32 Lives;
 	FString HiddenWord;
 	bool bGameOver;
